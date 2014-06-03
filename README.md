18c0f1a8b59289e9e6171c1bbb3038ed30599e45
NemSim <Nemanja@nemsim.com>
2014-06-03 12:15:51 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# 4 Hour Timeboxed Project: Movie Comparison
+
+A rails  webapp that can compare movies and the people that make them to show what they have in common. The project accomplishes these main 4 goals:
+
+* Using an IMDB API, allow search of movie or person and display information about it/them
+* allow comparidon of two movies to show any cast in common
+* allow comparison of two people to show any projects in common
+* allow comparison of person to movie to show if the person worked on that movie
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovieCompare::Application.config.secret_token = '8fbfac043124dcd629da94ae0df41db2aaa44971af9948d8488989f6400803c261c289f22a1bc4d9a58a3352b3f8798ca31b889fb1dd600e7624892dcf8ac93c'
